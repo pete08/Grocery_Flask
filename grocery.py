38c395ed3f3e00348b52a102b83973438e5f467d
pete08 <pete08@gmail.com>
2023-01-31 17:05:55 -0600

    Added url_for and register and login pages/routes, also added flash messages for user prompts when inputs are a scuccess or invalid

--- grocery.py
+++ grocery.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8cb9a5a7da9bc10f0a315302798154f1'
 
 store_items = [
     {
@@ -19,12 +22,32 @@ store_items = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
-    return render_template('home.html', store_items=store_items)
+def home():
+    return render_template('home.html', store_items = store_items)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title="About Page")
+
+@app.route("/register", methods=["POST","GET"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Registration", form=form)
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You\'ve been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Plase check suername and apssword', 'danger')
+    return render_template('login.html', title="Log In", form=form)
+
 
 
 if __name__ == '__main__':
