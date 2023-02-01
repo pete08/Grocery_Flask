e56a6b0c52a66a879ea4c555d801ea85ff678289
pete08 <pete08@gmail.com>
2023-02-01 17:40:56 -0600

    added login logout using flask-login module

--- grocery/routes.py
+++ grocery/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for, flash, redirect
-from grocery import app
+from flask import render_template, url_for, flash, redirect, request
+from grocery import app, db, bcrypt
 from grocery.forms import RegistrationForm, LoginForm
 from grocery.models import User, Item
+from flask_login import login_user, logout_user, current_user, login_required
 
 store_items = [
     {
@@ -30,19 +31,42 @@ def about():
 
 @app.route("/register", methods=["POST","GET"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title="Registration", form=form)
 
 @app.route("/login", methods=["POST","GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
             flash('You\'ve been logged in!', 'success')
-            return redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Plase check suername and apssword', 'danger')
+            flash('Login Unsuccessful. Plase check email and password', 'danger')
     return render_template('login.html', title="Log In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title="Account")
