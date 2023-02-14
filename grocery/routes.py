3cfb517d3d8ba867e0d8d64721dd8c0b42f10029
pete08 <pete08@gmail.com>
2023-02-14 12:55:18 -0600

    removed grocery/routes and grocery/forms due to prior creation of routes and forms in each separate blueprint grocery/items, grocery/users, grocery/main

--- grocery/routes.py
+++
@@ -1,9 +0,0 @@
-import os
-import secrets
-from PIL import Image
-from flask import render_template, url_for, flash, redirect, request, abort
-from grocery import app, db, bcrypt, mail
-from grocery.forms import RegistrationForm, LoginForm, UpdateAccountForm, NewItem, RequestResetPasswordForm, ResetPasswordForm
-from grocery.models import User, Item
-from flask_login import login_user, logout_user, current_user, login_required
-from flask_mail import Message
