bb9f52a38a43aeefb4620fea32872c074bed51bb
pete08 <pete08@gmail.com>
2023-02-16 17:11:02 -0600

    loops through home.html each users' items only displaying user's attributes at beginning of listed items. displays more of a list of grocery or household needs instead of a blog style

--- grocery/main/routes.py
+++ grocery/main/routes.py
@@ -13,10 +13,10 @@ def home():
     for i in userstrue:
         userstruelist_userids.append(i.id)
     page = request.args.get('page', 1, type=int)
-    itemspaginated = Item.query.filter(Item.user_id.in_(userstruelist_userids)).paginate(page=page, per_page=2)
+    itemspaginated = Item.query.filter(Item.user_id.in_(userstruelist_userids)).paginate(page=page, per_page=3)
     items_public = Item.query.filter(Item.user_id.in_(userstruelist_userids))
     # items = Item.query.order_by(Item.user_id.desc()).paginate(page=page, per_page=2)
-    return render_template('home.html', items=itemspaginated, users_ids=userstruelist_userids, items_public=items_public)
+    return render_template('home.html', items=itemspaginated, users_ids=userstruelist_userids, items_public=items_public, user_pics_used=[])
 
 @mainappblueprint.route("/about")
 def about():
